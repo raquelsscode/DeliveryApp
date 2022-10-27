bda3a6c2e0bba4c8f1ef1fb6c37cb8b5843d1692
David Costa <zevid.dev@outlook.com>
2022-10-27 19:16:40 -0300

    🐳 chore:cria seeders de products e users

---
+++ back-end/src/database/seeders/users.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert('users',
+    [
+      {
+        id:1, 
+        name:'Delivery App Admin', 
+        email:'adm@deliveryapp.com', 
+        password: '--adm2@21!!--', 
+        role: 'administrator'
+      },
+      {
+        id:2, 
+        name:'Fulana Pereira',
+        email:'fulana@deliveryapp.com', 
+        password:'fulana@123', 
+        role:'seller'
+      }, 
+      {
+        id:3,
+        name:'Cliente Zé Birita',
+        email:'zebirita@email.com',
+        password:'$#zebirita#$', 
+        role:'customer'
+      }
+    ], {}),
+
+  down: async (queryInterface) => queryInterface.bulkDelete('users', null, {}),
+};
