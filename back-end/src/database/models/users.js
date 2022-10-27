62fd8af2c3c0fc1169ef9fdf9f5f33b8cf3c055f
David Costa <zevid.dev@outlook.com>
2022-10-27 19:16:23 -0300

    ✨ feat:cria model de users

---
+++ back-end/src/database/models/users.js
@@ -0,0 +1,30 @@
+const UserModel = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    role: DataTypes.STRING
+  },
+  {
+    
+      timestamps: false,
+      tableName: 'users',
+      undescored: false
+  }
+  );
+
+  users.associate = (models) => {
+    users.hasMany(models.sales,
+      { foreignKey: 'userId' })
+  }
+
+  return users;
+};
+
+module.exports = UserModel;
