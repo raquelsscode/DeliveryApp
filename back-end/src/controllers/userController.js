8b30c848d55424d84eb043af6e5e8455e25f3d25
Amanda Lupoli <amandalppn@gmail.com>
2022-10-28 16:53:56 -0300

    Add route /login

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 const userService = require('../services/userService');
 
 const postLogin = async (req, res) => {
-    const { email, password } = req.body
+    const { email, password } = req.body;
     const product = await userService.postLogin(email, password);
     if (!product) {
       return res.status(400).json({ message: 'Error' });
     }
     return res.status(200).json(product);
-}
+};
 
-module.exports = { postLogin }
-
+module.exports = { postLogin };
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,15 +1,15 @@
-const {users} = require('../database/models');
+const { users } = require('../database/models');
 
 const postLogin = async (email, password) => {
     const getusers = await users.findOne({
         where: {
             email,
             password,
-        }
-    })
+        },
+    });
     return getusers;
-}
+};
 
 module.exports = { 
     postLogin,
-}
+};
