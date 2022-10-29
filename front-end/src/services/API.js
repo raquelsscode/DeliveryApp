aa04ec3bd4fd718fd7039c140d393e67122f70ac
Andre Santos <andrefelipe_santos@outlook.com>
2022-10-29 15:46:37 -0300

    implementa API para conectar frontend e backend.

---
+++ front-end/src/services/API.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+const API = axios.create({
+  baseURL: 'http://localhost:3001',
+});
+
+async function singIn(obj) {
+  const { data } = await API.post('/login', obj);
+  return data;
+}
+
+export {
+  singIn,
+};
