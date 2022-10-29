be50ba34b199a669f817ea9cf15c3ac9d8e372cc
David Costa <zevid.dev@outlook.com>
2022-10-29 11:25:07 -0300

    ✨ feat:aplica validação de e-mail e password

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const errorHandler = require('../middlewares/errorHandler');
+require('express-async-errors');
 const routes = require('../routes/routes');
 
 const app = express();
 app.use(express.json());
 app.use(routes);
 app.get('/coffee', (_req, res) => res.status(418).end());
-
+app.use(errorHandler);
 module.exports = app;
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,4 +1,20 @@
+const Joi = require('joi');
 const { users } = require('../database/models');
+const runSchema = require('../utils/runSchema');
+
+const MESSAGE_ERROR_LOGIN = 'Some required fields are missing';
+
+const loginValidate = runSchema(Joi.object({ 
+  email: Joi.string().email().required().messages({
+    'string.email': MESSAGE_ERROR_LOGIN,
+    'string.base': MESSAGE_ERROR_LOGIN,
+    'string.empty': MESSAGE_ERROR_LOGIN,
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.base': MESSAGE_ERROR_LOGIN,
+    'string.empty': MESSAGE_ERROR_LOGIN,
+  }),
+}));
 
 const postLogin = async (email, password) => {
     const getusers = await users.findOne({
@@ -11,5 +27,6 @@ const postLogin = async (email, password) => {
 };
 
 module.exports = { 
+    loginValidate,
     postLogin,
 };
