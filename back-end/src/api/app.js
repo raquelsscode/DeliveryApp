cac3aa63569204271aa9438d87a14d6578236668
Andre Santos <andrefelipe_santos@outlook.com>
2022-10-29 15:44:31 -0300

    chore: instala cors e implement uso no app.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const errorHandler = require('../middlewares/errorHandler');
 require('express-async-errors');
 const routes = require('../routes/routes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.get('/coffee', (_req, res) => res.status(418).end());
