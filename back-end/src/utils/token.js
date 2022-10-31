4c3129e490b61503a7a1ff6dd8cd5da397a5d56e
David Costa <zevid.dev@outlook.com>
2022-10-31 20:08:36 -0300

    ✨ feat:cria token jwt

---
+++ back-end/src/utils/token.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+require('dotenv/config');
+
+const { JWT_SECRET } = process.env;
+// const JWT_OPTIONS = {
+//   algorithm: 'HS256', 
+//   expiresIn: '1d', 
+// };
+
+const createToken = (payload) => {
+  const token = jwt.sign(payload, JWT_SECRET);
+  return token;
+};
+
+const verifyToken = (token) => {
+  const tokenVerify = jwt.verify(token, JWT_SECRET)
+  return tokenVerify
+}
+module.exports = {
+  createToken,
+  verifyToken
+};
