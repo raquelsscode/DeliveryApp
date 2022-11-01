1ddec8d254611cdc4904f22d8858f667dd036ad1
David Costa <zevid.dev@outlook.com>
2022-10-31 22:33:01 -0300

    🐞 fix:adiciona JWT_OPTIONS

--- back-end/src/utils/token.js
+++ back-end/src/utils/token.js
@@ -1,14 +1,14 @@
 const jwt = require('jsonwebtoken');
 require('dotenv/config');
 
-const { JWT_SECRET } = process.env;
-// const JWT_OPTIONS = {
-//   algorithm: 'HS256', 
-//   expiresIn: '1d', 
-// };
+const JWT_SECRET = process.env.JWT_SECRET || 'deliv3ry_4pP'
+const JWT_OPTIONS = {
+  expiresIn: '7d', 
+  algorithm: 'HS256', 
+};
 
 const createToken = (payload) => {
-  const token = jwt.sign(payload, JWT_SECRET);
+  const token = jwt.sign(payload, JWT_SECRET, JWT_OPTIONS);
   return token;
 };
 
