c2dde2b7fb9a7c36d4f7e7ee540333244faf6348
Andre Santos <andrefelipe_santos@outlook.com>
2022-11-01 21:44:36 -0300

    implementa rotas static para retornar imagens

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -7,7 +7,9 @@ const routes = require('../routes/routes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 app.use(routes);
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(errorHandler);
+
 module.exports = app;
