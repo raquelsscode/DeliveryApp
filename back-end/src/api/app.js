a690765aee74b2d448f1e8dbf0b993960b07d33a
David Costa <zevid.dev@outlook.com>
2022-11-03 14:38:57 -0300

    ✨ feat:adiciona express.static

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -7,6 +7,7 @@ const routes = require('../routes/routes');
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('public'));
 app.use(routes);
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use(errorHandler);
