9ae5c0d4c6e674813fa90057cebacf17e57d5e6f
Andre Santos <andrefelipe_santos@outlook.com>
2022-11-07 11:29:23 -0300

    🦄 refactor(refatora lo componente List):

--- front-end/src/componentes/List.js
+++ front-end/src/componentes/List.js
@@ -1,44 +1,50 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-export default function List() {
+export default function List({
+  product,
+  index,
+  onClick,
+}) {
   return (
     <tr>
       <td data-testid={ `customer_checkout__element-order-table-item-number-${index}` }>
         { index + 1 }
-
-        <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>
-          { order.name}
-        </td>
-
-        <td data-testid={ `customer_checkout__element-order-table-quantity-${index}` }>
-          { order.quantity }
-        </td>
-
-        <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }>
-          { price }
-        </td>
-
-        <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
-          { totaal }
-        </td>
       </td>
-      <Button
-        dataTestId={ `custom_checkout__element-order-table-remove-${index}` }
-        textButton="Remover"
-        onClick=""
-      />
+      <td data-testid={ `customer_checkout__element-order-table-name-${index}` }>
+        { product.name }
+      </td>
+
+      <td data-testid={ `customer_checkout__element-order-table-quantity-${index}` }>
+        { product.quantity }
+      </td>
+
+      <td data-testid={ `customer_checkout__element-order-table-unit-price-${index}` }>
+        { product.price.replace(/\./, ',') }
+      </td>
+
+      <td data-testid={ `customer_checkout__element-order-table-sub-total-${index}` }>
+        { (product.price * product.quantity).toFixed(2).replace(/\./, ',') }
+      </td>
+      
+      <td>
+        <Button
+          dataTestId={ `customer_checkout__element-order-table-remove-${index}` }
+          textButton="Remover"
+          onClick={ () => onClick(product.id) }
+        />
+      </td>
     </tr>
   );
 }
 
 List.propTypes = {
-  order: PropTypes.shape({
+  product: PropTypes.shape({
     name: PropTypes.string.isRequired,
     quantity: PropTypes.number.isRequired,
     id: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
+    price: PropTypes.string.isRequired,
     urlImage: PropTypes.string.isRequired,
   }).isRequired,
-  // index: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired,
 };
