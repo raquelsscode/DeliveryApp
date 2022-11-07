0cd22a7910df6868fc844ec2740dd57961f6cbdf
Andre Santos <andrefelipe_santos@outlook.com>
2022-11-07 11:28:47 -0300

    ✨ feat(implementa rota "/customer/checkout".):

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import LoginPage from './pages/LoginPage';
 import './App.css';
 import RegisterPage from './pages/RegisterPage';
 import ProductsPage from './pages/ProductsPage';
+import CheckoutPage from './pages/CheckoutPage';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={ <Navigate to="/login" /> } />
       <Route path="/register" element={ <RegisterPage /> } />
       <Route path="/customer/products" element={ <ProductsPage /> } />
+      <Route path="/customer/checkout" element={ <CheckoutPage /> } />
     </Routes>
   );
 }
