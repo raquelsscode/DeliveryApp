970393110fc42cd3a43712493d59cf6c78eb225d
Andre Santos <andrefelipe_santos@outlook.com>
2022-11-08 16:29:36 -0300

    🦄 refactor(refatora OrederDetilsLIne.):
    insere os data-testeid's e implementa o uso da biblioteca moment.

--- front-end/src/componentes/OrderDetailsLine.js
+++ front-end/src/componentes/OrderDetailsLine.js
@@ -1,31 +1,41 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
+import moment from 'moment/moment';
 
 export default function OrderDetailsLine({
   order,
 }) {
   return (
     <tr>
-      <td>
+      <td
+        data-testid='customer_order_details__element-order-details-label-order-id'
+      >
         { `Pedido ${order.id}` }
       </td>
 
-      <td>
-        { `P.Vend: ${order.sellerId}` }
+      <td
+        data-testid='customer_order_details__element-order-details-label-seller-name'
+      >
+        { `P.Vend: ${order.seller.name}` }
       </td>
 
-      <td>
-        { order.date }
+      <td
+        data-testid='customer_order_details__element-order-details-label-order-date'
+      >
+        { moment(order.saleDate).utc().format('DD/MM/YYYY') }
       </td>
 
-      <td>
+      <td
+        data-testid='customer_order_details__element-order-details-label-delivery-status<index>'
+      >
         { order.status }
       </td>
 
       <td>
         <Button
+          dataTestId='customer_order_details__button-delivery-check'
           textButton="Marcar com entregue"
-
+          isDisabled={ true }
         />
       </td>
     </tr>
